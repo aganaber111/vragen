d1585e4dae247c53cde1e13cda06cba4c0b42b42
aganaber111 <ardaofarda81@gmail.com>
2024-12-18 00:56:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-document.getElementById('toggle-button').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-    
-    // Sembolü değiştir
-    const icon = document.getElementById('toggle-icon');
-    if (document.body.classList.contains('dark-mode')) {
-        icon.innerHTML = '☀️'; // Koyu moddayken sembol güneş
-    } else {
-        icon.innerHTML = '🌙'; // Normal moddayken sembol ay
-    }
-});
+
